73dba86f4729f7b16895a0bd54e90b341d6ea91b
Shlomo <shlomo@fundit.io>
2022-03-13 22:59:18 +0200

    update dto validation

--- src/tasks/dto/task-filter-dto.ts
+++ src/tasks/dto/task-filter-dto.ts
@@ -1,6 +1,12 @@
+import { IsEmpty, IsIn, IsOptional } from 'class-validator';
 import { TaskStatus } from '../task.module';
 
 export class GetTaskFilterDto {
+  @IsOptional()
+  @IsIn([TaskStatus.DONE, TaskStatus.OPEN, TaskStatus.IN_PROGRESS])
   status: TaskStatus;
+
+  @IsOptional()
+  @IsEmpty()
   search: string;
 }
